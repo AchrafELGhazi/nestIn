e3c36e66ca9e1bf161964f17a30fbae0a3696caf
AchrafElGhazi <a.elghazi@aui.ma>
2025-05-28 15:44:49 +0100

    feat: Update project structure, including layout adjustments

--- web/src/components/map/Map.jsx
+++ web/src/components/map/Map.jsx
@@ -9,7 +9,9 @@ function Map({items}){
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+      />
+      
+      {/* change map provider */}
     {items.map(item=>(
       <Pin item={item} key={item.id}/>
     ))}

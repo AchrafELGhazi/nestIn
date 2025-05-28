e021a262f28a2a85c7c17aef90c0cdbaae286cbb
AchrafElGhazi <a.elghazi@aui.ma>
2025-05-28 22:17:05 +0100

    style: Refactor SCSS for Navbar and ProfilePage components, improving layout and responsiveness

--- web/src/routes/profilePage/profilePage.jsx
+++ web/src/routes/profilePage/profilePage.jsx
@@ -1,45 +1,47 @@
-import Chat from "../../components/chat/Chat";
-import List from "../../components/list/List";
-import "./profilePage.scss";
+import Chat from '../../components/chat/Chat';
+import List from '../../components/list/List';
+import './profilePage.scss';
 
 function ProfilePage() {
   return (
-    <div className="profilePage">
-      <div className="details">
-        <div className="wrapper">
-          <div className="title">
+    <div className='profilePage'>
+      <div className='details'>
+        <div className='wrapper'>
+          <div className='title'>
             <h1>User Information</h1>
             <button>Update Profile</button>
           </div>
-          <div className="info">
-            <span>
-              Avatar:
+          <div className='info'>
+            <div className='info-item'>
+              <span className='label'>Avatar:</span>
               <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
+                src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                alt='User Avatar'
               />
-            </span>
-            <span>
-              Username: <b>John Doe</b>
-            </span>
-            <span>
-              E-mail: <b>john@gmail.com</b>
-            </span>
+            </div>
+            <div className='info-item'>
+              <span className='label'>Username:</span>
+              <span className='value'>John Doe</span>
+            </div>
+            <div className='info-item'>
+              <span className='label'>E-mail:</span>
+              <span className='value'>john@gmail.com</span>
+            </div>
           </div>
-          <div className="title">
+          <div className='title'>
             <h1>My List</h1>
             <button>Create New Post</button>
           </div>
           <List />
-          <div className="title">
+          <div className='title'>
             <h1>Saved List</h1>
           </div>
           <List />
         </div>
       </div>
-      <div className="chatContainer">
-        <div className="wrapper">
-          <Chat/>
+      <div className='chatContainer'>
+        <div className='wrapper'>
+          <Chat />
         </div>
       </div>
     </div>
